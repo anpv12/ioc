cdefaeee7f1e13ae928292bcf47e3e5f4fc62b63
Trinh-Nguyen-Ngoc-Tu <nguyenngoctutrinh92@gmail.com>
2026-07-23 17:11:00 +0700

    clean: remove recover.ps1 diagnostic script

--- recover.ps1
+++
@@ -1,5 +0,0 @@
-$commits = git log --all --format="%H" -- "pages/Quản trị/style.css"
-foreach ($c in $commits) {
-    $lines = (git show "${c}:pages/Quản trị/style.css" | Measure-Object -Line).Lines
-    Write-Host "Commit $c - Lines: $lines"
-}
